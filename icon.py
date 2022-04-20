7b6067dc5a29d87333fa523bae640cb261993ada
Igor Maljuk <tigormal@yandex.ru>
2022-04-20 03:43:15 +0300

    Initial commit, added base classes

---
+++ icon.py
@@ -0,0 +1,104 @@
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+class GadgetIcon(QWidget):
+    def __init__(self, icon, marks = False):
+        super().__init__()
+
+        self.marks = marks
+        self.icon = QPixmap(icon)
+        self.track_marks = QPixmap(r'TrackingMarks.png')
+        self.effect = QGraphicsColorizeEffect(self)
+        self.effect.setColor(QColor("White"))
+        self.setGraphicsEffect(self.effect)
+        self.setFixedSize(QSize(58,58))
+        self.setSizePolicy(
+            QSizePolicy.MinimumExpanding,
+            QSizePolicy.MinimumExpanding
+        )
+
+    def sizeHint(self):
+        return QSize(58,58)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        if self.marks:
+            rect = QRect(0,0,painter.device().width(), painter.device().height())
+            painter.drawPixmap(rect, self.track_marks)
+        rect = QRect(13, 13, 32, 32)
+        painter.drawPixmap(rect, self.icon)
+
+    def showMarks(self, val):
+        self.marks = val
+
+
+class MapElement(QWidget):
+
+    name = ""
+    tags = []
+    icon_file = ""
+    isTracked = False
+    tint_color = QColor("White")
+
+    clicked = Signal()
+
+    def __init__(self, name = "", tags = [], icon_file="mcu.png", isTracked = False):
+        super().__init__()
+
+        self.setSizePolicy(
+            QSizePolicy.MinimumExpanding,
+            QSizePolicy.MinimumExpanding
+        )
+
+        self.name = name
+        self.tags = tags
+        self.icon_file = icon_file
+        self.isTracked = isTracked
+
+        self.icon = GadgetIcon(self.icon_file, marks=self.isTracked)
+        self.lbl = QLabel(self.name)
+        self.lbl.setAlignment(Qt.AlignCenter)
+        self.lbl.setFont(QFont('PT Sans', 14))
+        tagline = ""
+        for i in self.tags:
+            tagline = tagline + str(i) + ', ' 
+        self.sublbl = QLabel()
+        self.sublbl.setText(tagline)
+        self.sublbl.setAlignment(Qt.AlignCenter)
+        self.sublbl.setFont(QFont('PT Sans', 10))
+
+        self.setFixedSize(QSize(70,100))
+
+        l = QVBoxLayout()
+        l.addWidget(self.icon)
+        l.addWidget(self.lbl)
+        l.addWidget(self.sublbl)
+        self.setLayout(l)
+
+
+    def sizeHint(self):
+        return QSize(70, 100)
+
+    def refresh(self):
+        self.lbl.setText(self.name)
+        tagline = ""
+        for i in self.tags:
+            tagline = tagline + str(i) + ', '
+        self.sublbl.setText(tagline)
+        self.icon.showMarks(self.isTracked)
+
+
+    def mousePressEvent(self, e):
+        self.clicked.emit()
+        
+
+if __name__ == '__main__':
+    import sys
+    
+    app = QApplication(sys.argv)
+
+    widget = MapElement(name="Lamp", tags=["Kitchen", "RGB"], icon_file="lamp.png", isTracked=True)
+    widget.show()
+
+    app.exec_()
